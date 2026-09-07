fe80f8c4bb16e2ca99b5ac6f6f5b1084c5b45f4c
bluelily07 <maxx.powsjal@gmail.com>
2026-09-07 03:10:19 -0700

    main template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define intmax INT_MAX
+#define intmin INT_MIN
+#define str string
+#define int128 __int128
+#define mod 998244353
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int _;
+    cin >> _;
+    while (_--) solve();
+}
